1f038eb8c06208eca9563843d5025aa20abc5ad1
VindexTOS <giorgikutateladze1998@gmail.com>
2023-05-27 19:16:09 +0400

    merg

---
+++ server/SigmaMovies/SigmaMovies.Application/HelperModels/SortBy.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SigmaMovies.Application.HelperModels
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum SortBy
+    {
+        Year,
+        IMDbRating,
+        RTRating
+    }
+}
